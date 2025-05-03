d140e5ff3792fbc4b98ad57f5c61966c5e421e87
Sara Ashraf <saraashraf1822004@gmail.com>
2025-05-04 00:47:34 +0300

    feat: Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,17 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+    implementation("io.insert-koin:koin-core:4.0.2")
     testImplementation(kotlin("test"))
+
+    // tests
+    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.1")
+    testImplementation("io.mockk:mockk:1.13.16")
+
+    // coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 }
 
 tasks.test {
