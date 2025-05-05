879fa19d36dc781bb75b0ec5d89de57006edc945
am17jx <ameerahmed0780@gmail.com>
2025-05-05 18:59:27 +0300

    Edit : delete commint

--- src/main/kotlin/data/repository/GetCoordinatesFromCityRepositoryImpl.kt
+++ src/main/kotlin/data/repository/GetCoordinatesFromCityRepositoryImpl.kt
@@ -6,7 +6,7 @@ import org.example.logic.model.Coordinates
 class GetCoordinatesFromCityRepositoryImpl : GetCoordinatesFromCityRepository {
     override fun getCoordinatesForCity(city: String): Coordinates? {
         return when (city.lowercase()) {
-            //fake data
+
             "cairo" -> Coordinates(30.0444, 31.2357)
             "london" -> Coordinates(51.5074, -0.1278)
             else -> null
