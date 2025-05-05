d916c1bc3fb8844edceea7c76174aa4f356f1c2d
Sara Ashraf <saraashraf1822004@gmail.com>
2025-05-06 00:53:03 +0300

    feat: add GetCurrentLocationUseCase

---
+++ src/main/kotlin/logic/useCase/GetCurrentLocationUseCase.kt
@@ -0,0 +1,11 @@
+package org.example.logic.useCase
+
+import org.example.logic.model.Coordinates
+import org.example.logic.repository.LocationRepository
+
+class GetCurrentLocationUseCase(
+    private val locationRepository: LocationRepository
+) {
+    suspend operator fun invoke(): Result<Coordinates> =
+        locationRepository.getCurrentLocation()
+}
