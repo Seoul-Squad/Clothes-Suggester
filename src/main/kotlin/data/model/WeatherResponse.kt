77c3fc6d8f7aee3ac6b137fcd3243e9953bf2f04
Mohamed Hesham <medo93346@gmail.com>
2025-05-06 20:06:17 +0300

    add Serializable annotation for WeatherResponse class

--- src/main/kotlin/data/model/WeatherResponse.kt
+++ src/main/kotlin/data/model/WeatherResponse.kt
@@ -1,7 +1,9 @@
 package data.model
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class WeatherResponse(
     @SerialName("temperature_2m") val temperature: Double,
     @SerialName("weather_code")val weatherCode: Int
