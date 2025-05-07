6a97b9f1cea2941f65af8848ba3f81d8e09a7200
Sara Ashraf <saraashraf1822004@gmail.com>
2025-05-07 14:19:11 +0300

    refactor: remove coroutine scope.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,27 @@
 package org.example
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import org.example.data.dataSource.IpInfoLocationParser
+import org.example.data.dataSource.KtorIpLocationApi
 import org.example.data.repository.LocationRepositoryImpl
 import org.example.logic.useCase.GetCurrentLocationUseCase
 
 fun main() = runBlocking(Dispatchers.IO) {
-    val repository = LocationRepositoryImpl()
-    val useCase = GetCurrentLocationUseCase(repository, dispatcher = Dispatchers.IO)
+    val client = HttpClient(CIO)
+    val api = KtorIpLocationApi(client)
+    val parser = IpInfoLocationParser()
+    val repository = LocationRepositoryImpl(api, parser)
+    val useCase = GetCurrentLocationUseCase(repository)
+
     try {
         val cords = useCase()
         println("Current location: Latitude=${cords.latitude}, Longitude=${cords.longitude}")
     } catch (e: Exception) {
         println("Failed to fetch location: ${e.message}")
+    } finally {
+        client.close()
     }
 }
--- src/main/kotlin/logic/useCase/GetCurrentLocationUseCase.kt
+++ src/main/kotlin/logic/useCase/GetCurrentLocationUseCase.kt
@@ -1,18 +1,11 @@
 package org.example.logic.useCase
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import org.example.logic.model.Coordinates
 import org.example.logic.repository.LocationRepository
 
 class GetCurrentLocationUseCase(
     private val locationRepository: LocationRepository,
-    private val dispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(): Coordinates = coroutineScope {
-        async(dispatcher) {
-            locationRepository.getCurrentLocation()
-        }.await()
-    }
+    suspend operator fun invoke(): Coordinates =
+        locationRepository.getCurrentLocation()
 }
